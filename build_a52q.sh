440b8cb0fdb2ca1f99e7ddd3d84a08fe86f63298
Simon1511 <me@simon1511.de>
2023-10-25 21:33:25 +0200

    Adapt build scripts
    Change-Id: I24a4f8c29a716d7cbe808b75c77aa4746ce84206
    Signed-off-by: Ruchit <risenid@duck.com>

--- build_a52q.sh
+++ build_a52q.sh
@@ -6,4 +6,6 @@ export ARCH=arm64
 
 make ARCH=arm64 O=out vendor/a52q_eur_open_defconfig
 
-make ARCH=arm64 O=out -j16 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi-
+make ARCH=arm64 O=out -j24 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi-
+
+# tools/mkdtimg create arch/arm64/boot/dtbo.img --page_size=4096 $(find arch -name "*.dtbo")
