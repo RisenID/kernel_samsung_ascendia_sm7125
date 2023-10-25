a66780d07bd2f5dfd4e3c96bb3bb65b497acdf5f
Simon1511 <me@simon1511.de>
2023-10-25 21:33:25 +0200

    Adapt build scripts
    Change-Id: I24a4f8c29a716d7cbe808b75c77aa4746ce84206

--- build_a72q.sh
+++ build_a72q.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-PATH="../toolchains/clang/clang-r383902/bin:../toolchains/gcc/aarch64/bin:../toolchains/gcc/arm/bin:${PATH}"
+PATH="../../toolchains/clang/clang-r383902/bin:../../toolchains/gcc/aarch64/bin:../../toolchains/gcc/arm/bin:${PATH}"
 
 export ARCH=arm64
 
-make ARCH=arm64 vendor/a72q_eur_open_defconfig
+make ARCH=arm64 O=out vendor/a72q_eur_open_defconfig
 
-make ARCH=arm64 -j16 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi-
+make ARCH=arm64 O=out -j24 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi-
+
+# tools/mkdtimg create arch/arm64/boot/dtbo.img --page_size=4096 $(find arch -name "*.dtbo")
