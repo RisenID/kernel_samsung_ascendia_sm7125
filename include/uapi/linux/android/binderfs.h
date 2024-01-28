6b098245ca66d280b02df62b37cc56e1713e18c7
Tim Zimmermann <tim@linux4.de>
2024-01-28 07:20:56 +0100

    android: Checkout binder to 4.14.190
    * To get rid of samsung's modifications
    
    Change-Id: I2d39974fee21b5178c17e161e6d9a49dd75ded33
    Signed-off-by: Ruchit <risen@pixelexperience.org>

--- include/uapi/linux/android/binderfs.h
+++ include/uapi/linux/android/binderfs.h
@@ -22,8 +22,8 @@
  */
 struct binderfs_device {
 	char name[BINDERFS_MAX_NAME + 1];
-	__u32 major;
-	__u32 minor;
+	__u8 major;
+	__u8 minor;
 };
 
 /**
@@ -32,4 +32,3 @@ struct binderfs_device {
 #define BINDER_CTL_ADD _IOWR('b', 1, struct binderfs_device)
 
 #endif /* _UAPI_LINUX_BINDER_CTL_H */
-
