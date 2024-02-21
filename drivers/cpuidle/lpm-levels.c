e9811c5f09471787f02ab415d0dc55883182946a
Ruchit <risenid@duck.com>
2024-02-21 13:16:49 +0530

    cpuidle: drop some samsung debug logging
    drivers/cpuidle/lpm-levels.o: In function `lpm_suspend_prepare':
    /home/risen/android/ascendia/out/../drivers/cpuidle/lpm-levels.c:1750: undefined reference to `debug_masterstats_show'
    /home/risen/android/ascendia/out/../drivers/cpuidle/lpm-levels.c:1751: undefined reference to `debug_rpmstats_show'
    drivers/cpuidle/lpm-levels.o: In function `lpm_suspend_wake':
    /home/risen/android/ascendia/out/../drivers/cpuidle/lpm-levels.c:1773: undefined reference to `debug_rpmstats_show'
    /home/risen/android/ascendia/out/../drivers/cpuidle/lpm-levels.c:1774: undefined reference to `debug_masterstats_show'
    make[1]: *** [/home/risen/android/ascendia/Makefile:1190: vmlinux] Error 1

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -1746,9 +1746,6 @@ static int lpm_suspend_prepare(void)
 #ifdef CONFIG_SEC_PM
 	regulator_showall_enabled();
 	sec_clock_debug_print_enabled();
-
-	debug_masterstats_show("entry");
-	debug_rpmstats_show("entry");
 #endif
 
 #ifdef CONFIG_SEC_PM_DEBUG
@@ -1770,8 +1767,6 @@ static void lpm_suspend_wake(void)
 
 #ifdef CONFIG_SEC_PM
 	sec_debug_print_sleep_time();
-	debug_rpmstats_show("exit");
-	debug_masterstats_show("exit");
 #endif
 }
 
