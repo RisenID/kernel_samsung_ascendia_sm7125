2989e6b34173c30aea49c19e1af7a496d3d21445
Ruchit <risen@pixelexperience.org>
2024-03-13 17:48:14 +0530

    Ascendia 3.1

--- build_a72q.sh
+++ build_a72q.sh
@@ -1,9 +1,47 @@
 #!/bin/bash
 
+if [ -e KernelSU/ ]
+then
+    echo " "
+    echo " Deleteting KernelSU/ "
+    echo " "
+    rm -rf KernelSU
+fi
+
+if [ -e out/ ]
+then
+    echo " "
+    echo " Deleteting out/ "
+    echo " "
+    rm -rf out
+fi
+
 PATH="../../toolchains/clang/clang-r383902/bin:../../toolchains/gcc/aarch64/bin:../../toolchains/gcc/arm/bin:${PATH}"
 
 export ARCH=arm64
 
 make ARCH=arm64 O=out vendor/a72q_eur_open_defconfig
 
-make ARCH=arm64 O=out -j16 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi-
+make ARCH=arm64 O=out -j24 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi-
+
+echo " "
+echo "Build Complete!"
+echo " "
+
+rm ../builds/ascendia/a72q/oneui_vanilla/dtb
+rm ../builds/ascendia/a72q/oneui_vanilla/kernel
+
+cp out/arch/arm64/boot/Image ../builds/ascendia/a72q/oneui_vanilla/kernel
+cp out/arch/arm64/boot/dts/qcom/atoll-ab-idp.dtb ../builds/ascendia/a72q/oneui_vanilla/dtb
+
+rm -rf out/
+
+echo " "
+echo "Making boot image"
+echo " "
+
+cd ../builds/ascendia/a72q/oneui_vanilla/
+magiskboot_x86 repack boot.img Ascendia_3.1_Vanilla_OneUI_a72q_boot.img
+
+rm ../../pack_vanilla/ascendia/a72/oneui.img
+cp Ascendia_3.1_Vanilla_OneUI_a72q_boot.img ../../pack_vanilla/ascendia/a72/oneui.img
