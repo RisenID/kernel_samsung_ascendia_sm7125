40b584f9d5abfe8c2ea26aa1fa6a662dd6acb821
Ruchit <risen@pixelexperience.org>
2024-03-13 17:48:14 +0530

    Ascendia 3.1

--- build_a52q.sh
+++ build_a52q.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ -e out/ ]
+then
+    echo " "
+    echo " Deleteting out/ "
+    echo " "
+    rm -rf out
+fi
+
 PATH="../../toolchains/clang/clang-r383902/bin:../../toolchains/gcc/aarch64/bin:../../toolchains/gcc/arm/bin:${PATH}"
 
 export ARCH=arm64
@@ -8,4 +16,24 @@ make ARCH=arm64 O=out vendor/a52q_eur_open_defconfig
 
 make ARCH=arm64 O=out -j24 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi-
 
-# tools/mkdtimg create arch/arm64/boot/dtbo.img --page_size=4096 $(find arch -name "*.dtbo")
+echo " "
+echo "Build Complete!"
+echo " "
+
+rm ../builds/ascendia/a52q/aosp_ksu/dtb
+rm ../builds/ascendia/a52q/aosp_ksu/kernel
+
+cp out/arch/arm64/boot/Image ../builds/ascendia/a52q/aosp_ksu/kernel
+cp out/arch/arm64/boot/dts/qcom/atoll-ab-idp.dtb ../builds/ascendia/a52q/aosp_ksu/dtb
+
+rm -rf out/
+
+echo " "
+echo "Making boot image"
+echo " "
+
+cd ../builds/ascendia/a52q/aosp_ksu/
+magiskboot_x86 repack boot.img Ascendia_3.1_KSU_AOSP_a52q_boot.img
+
+rm ../../pack_ksu/ascendia/a52/aosp.img
+cp Ascendia_3.1_KSU_AOSP_a52q_boot.img ../../pack_ksu/ascendia/a52/aosp.img
--- build_a72q.sh
+++ build_a72q.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ -e out/ ]
+then
+    echo " "
+    echo " Deleteting out/ "
+    echo " "
+    rm -rf out
+fi
+
 PATH="../../toolchains/clang/clang-r383902/bin:../../toolchains/gcc/aarch64/bin:../../toolchains/gcc/arm/bin:${PATH}"
 
 export ARCH=arm64
@@ -8,4 +16,24 @@ make ARCH=arm64 O=out vendor/a72q_eur_open_defconfig
 
 make ARCH=arm64 O=out -j24 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi-
 
-# tools/mkdtimg create arch/arm64/boot/dtbo.img --page_size=4096 $(find arch -name "*.dtbo")
+echo " "
+echo "Build Complete!"
+echo " "
+
+rm ../builds/ascendia/a72q/aosp_ksu/dtb
+rm ../builds/ascendia/a72q/aosp_ksu/kernel
+
+cp out/arch/arm64/boot/Image ../builds/ascendia/a72q/aosp_ksu/kernel
+cp out/arch/arm64/boot/dts/qcom/atoll-ab-idp.dtb ../builds/ascendia/a72q/aosp_ksu/dtb
+
+rm -rf out/
+
+echo " "
+echo "Making boot image"
+echo " "
+
+cd ../builds/ascendia/a72q/aosp_ksu/
+magiskboot_x86 repack boot.img Ascendia_3.1_KSU_AOSP_a72q_boot.img
+
+rm ../../pack_ksu/ascendia/a72/aosp.img
+cp Ascendia_3.1_KSU_AOSP_a72q_boot.img ../../pack_ksu/ascendia/a72/aosp.img
