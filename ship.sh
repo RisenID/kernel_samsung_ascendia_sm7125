e2752326d82cd27781ce79500b65f734d9fc71be
Ruchit <risenid@duck.com>
2024-04-28 15:34:41 +0530

    Ascendia 3.3
    Change-Id: Icdd7062ba42a209a6eceb1491987d4088eed357c
    Signed-off-by: Ruchit <risenid@duck.com>

--- ship.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ../../builds/ascendia/sm7125/pack_ksu/
-sed -i -e 's/3.1/3.2/g' META-INF/com/google/android/update-binary
-zip -r Ascendia_3.2_KSU_a572q.zip ascendia META-INF
-mv Ascendia_3.2_KSU_a572q.zip ../v3/3.2/Ascendia_3.2_KSU_a572q.zip
