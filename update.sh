c705d6c7449341f547503ce9240ec92de509e142
Ruchit <risenid@duck.com>
2024-04-28 15:34:41 +0530

    Ascendia 3.3
    Change-Id: Icdd7062ba42a209a6eceb1491987d4088eed357c
    Signed-off-by: Ruchit <risenid@duck.com>

--- update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sed -i -e 's/3\.1/3\.2/g' a52q.sh a72q.sh ship.sh arch/arm64/configs/vendor/a52q_eur_open_defconfig arch/arm64/configs/vendor/a72q_eur_open_defconfig
-sed "4 c\sed -i -e 's\/3.1\/3.2\/g' META-INF\/com\/google\/android\/update-binary" ship.sh > ship
-rm -f ship.sh
-mv ship ship.sh
-chmod +x ship.sh
